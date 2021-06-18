d8e4acf775f9071f1170f349c99190d2d4b65aa6
WonWonGit <bkn020612@gmail.com>
2021-06-18 18:06:03 +0900

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+  import firebase from "firebase/app";
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyC4vFS9thWw_FuJolmcsPlM-zTWAd_aArQ",
+    authDomain: "wonwitter-e7621.firebaseapp.com",
+    projectId: "wonwitter-e7621",
+    storageBucket: "wonwitter-e7621.appspot.com",
+    messagingSenderId: "109231183569",
+    appId: "1:109231183569:web:b32623014cf699732697f0",
+    measurementId: "G-DTJEP34BEH"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
