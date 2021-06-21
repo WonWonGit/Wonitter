85d5d5b561509a03a4fae1bd96c56d6a8be45503
WonWonGit <bkn020612@gmail.com>
2021-06-21 11:49:04 +0900

    log in

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
 import {authService} from 'fbInstance';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-  return <AppRouter isLoggedIn={isLoggedIn}/>;
+  const[init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(()=>{
+    authService.onAuthStateChanged((user)=>{
+      if(user){
+        setIsLoggedIn(true);
+      }else{
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+    
+  },[])
+  return (
+  <>
+    {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initializing..."}
+    <footer>&copy; {new Date().getFullYear()}wonwitter</footer>
+  </>)
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (event) => {
     const{target: {name, value},} =event;
     if(name === "email"){
@@ -24,9 +25,10 @@ const Auth = () => {
         }
         console.log(data);
     }catch(error){
-        console.log(error);
+        setError(error.message);
     }
   };
+  const toggleAccout = () => setNewAccount(prev => !prev);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -51,7 +53,9 @@ const Auth = () => {
           <button>Continue with Google</button>
           <button>Continue with Github</button>
         </div>
+        {error}
       </form>
+      <span onClick={toggleAccout}>{newAccount ? "Sign in":"Create Account"}</span>
     </div>
   );
 };
