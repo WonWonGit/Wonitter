57b330b79aa6f70460e057fb37b614899eabc918
WonWonGit <bkn020612@gmail.com>
2021-06-22 18:02:37 +0900

    구글 깃허브 로그인

--- src/fbInstance.js
+++ src/fbInstance.js
@@ -13,4 +13,5 @@
 
 firebase.initializeApp(firebaseConfig);
 
+export const firebaseInstance = firebase;
 export const authService = firebase.auth();
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { authService } from "../fbInstance";
+import { authService, firebaseInstance } from "../fbInstance";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
@@ -29,6 +29,17 @@ const Auth = () => {
     }
   };
   const toggleAccout = () => setNewAccount(prev => !prev);
+  const onSocailClick = async (event) => {
+      const {target:{name}} = event;
+      let provider;
+      if(name === "google"){
+          provider = new firebaseInstance.auth.GoogleAuthProvider();
+      }else if(name === "github"){
+          provider = new firebaseInstance.auth.GithubAuthProvider();
+      }
+      const data = await authService.signInWithPopup(provider);
+      console.log(data);
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -49,12 +60,12 @@ const Auth = () => {
           onChange={onChange}
         />
         <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        </form>
         <div>
-          <button>Continue with Google</button>
-          <button>Continue with Github</button>
+          <button onClick={onSocailClick} name="google">Continue with Google</button>
+          <button onClick={onSocailClick} name="github">Continue with Github</button>
         </div>
         {error}
-      </form>
       <span onClick={toggleAccout}>{newAccount ? "Sign in":"Create Account"}</span>
     </div>
   );
