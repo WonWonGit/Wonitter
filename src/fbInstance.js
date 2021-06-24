88d8b4fbab9fac0ba59563eb92cfb5efcec2b841
WonWonGit <bkn020612@gmail.com>
2021-06-24 20:52:40 +0900

    firestore

--- src/fbInstance.js
+++ src/fbInstance.js
@@ -1,5 +1,6 @@
   import firebase from "firebase/app";
   import "firebase/auth";
+  import "firebase/firestore";
 
   var firebaseConfig = {
     apiKey: "AIzaSyC4vFS9thWw_FuJolmcsPlM-zTWAd_aArQ",
@@ -15,3 +16,4 @@ firebase.initializeApp(firebaseConfig);
 
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
